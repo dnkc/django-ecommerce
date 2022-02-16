518edaf5e2d8a88a35e8824eb462ad0ef053948f
dnkc <dnkcdesign@gmail.com>
2022-02-15 23:14:41 -0500

    added account login, register, logout

--- accounts/admin.py
+++ accounts/admin.py
@@ -14,8 +14,8 @@ class AccountAdmin(UserAdmin):
         'is_active',
         'date_joined',
     )
-    list_display_links = ('email', 'first_name', 'last_name')
-    readonly_fields = ('last_login', 'date_joined')
+    list_display_links = ('email', 'first_name', 'last_name',)
+    readonly_fields = ('last_login', 'date_joined',)
     ordering = ("-date_joined",)
     
     filter_horizontal = ()
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from accounts.forms import RegistrationForm
-
+from accounts.models import Account
+from django.contrib import messages
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Registration successful.')
+            return redirect('register')
+    else:
+        form = RegistrationForm()
+
     context = {
         'form': form,
     }
     return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        #username = Account.objects.get(email=email).username
+
+        user = auth.authenticate(email=email, password=password)
+        print(user)
+        if user is not None:
+            auth.login(request, user)
+            
+            #messages.success(request, "Login successful.")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid login credentials.")
+            return redirect('login')
+
     return render(request, 'accounts/login.html')
 
+@login_required(login_url = 'login') 
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, "You are logged out.")
+    return redirect('login')
