be21545089eaa095e6a219c819c492b77cf93920
dnkc <dnkcdesign@gmail.com>
2022-02-19 20:54:37 -0500

    finished review & rating system

--- superkart/settings.py
+++ superkart/settings.py
@@ -121,13 +121,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'superkart/static',
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # media files configuration
